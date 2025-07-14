d2eaf14b95d456d27bd742c3a51d92f77c395ffc
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-07-14 17:27:03 -0300

    Excluindo ChaveApi.

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
-//AIzaSyDHxaU_088cQxEu_Z4Yyl6nuOqRdJf_AhU
-
 const markdownToHTML = (text) => {
     const converter = new showdown.Converter()
     return converter.makeHtml(text)
